01770edabd0245774f65f714c9d2520fb251a13e
Kihyun Lim <dlarlgus0619@gmail.com>
2026-07-01 00:22:08 +0900

    chore: 사용하지 않던 상수 사용

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -12,7 +12,7 @@ public class CarName {
     private void validate(String name){
         if(name == null)
             throw new IllegalArgumentException("빈 이름은 불가능합니다.");
-        else if(name.length() > 5)
+        else if(name.length() > MAX_LENGTH)
             throw new IllegalArgumentException("이름은 다섯 글자를 초과할 수 없습니다.");
     }
 
